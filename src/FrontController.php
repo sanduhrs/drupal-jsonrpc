ee79bc374a8377910a9a4f4c838a349965e9f55d
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-04-13 21:59:30 +0200

    feat: return valid JSON-RPC structure

--- src/FrontController.php
+++ src/FrontController.php
@@ -4,9 +4,13 @@ namespace Drupal\jsonrpc;
 
 
 use Drupal\Component\Serialization\Json;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Handles all the RPC endpoints.
+ */
 class FrontController {
 
   /**
@@ -20,6 +24,7 @@ class FrontController {
    */
   public function handle(Request $request) {
     // TODO: Create custom exceptions for errors handled by the subscriber.
+    // TODO: Allow passing the JSON object encoded in the URL as a query string parameter. Then make sure we can leverage page cache in the appropriate situations.
     if ($request->getMethod() !== Request::METHOD_POST) {
       return new Response('{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Invalid Request"}, "id": null}');
     }
@@ -29,7 +34,18 @@ class FrontController {
     }
     // TODO: Inject plugin manager properly.
     $rpc_request = RpcRequest::create($parsed_body, \Drupal::service('plugin.manager.rpc_endpoint'));
-    return new Response($rpc_request->getEndpoint()->execute());
+    $result = $rpc_request->getEndpoint()->execute();
+    $output = [
+      'jsonrpc' => '2.0',
+      'result' => $result,
+      'id' => $rpc_request->getId(),
+    ];
+
+    // If there is no ID we don't provide a response. This is called
+    // "notification" in the JSON-RPC spec.
+    return $rpc_request->getId()
+      ? new JsonResponse($output)
+      : new JsonResponse();
   }
 
 }
