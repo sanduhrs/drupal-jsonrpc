00c4bd42b4d01668b996b4f990299c08fbc415ff
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-04-21 07:06:49 +0200

    feat: add permission to role method plugin

---
+++ modules/jsonrpc_core/src/Plugin/jsonrpc/Method/AddPermissionToRole.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\jsonrpc_core\Plugin\jsonrpc\Method;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\jsonrpc\Annotation\JsonRpcMethod;
+use Drupal\jsonrpc\Annotation\JsonRpcParameter;
+use Drupal\jsonrpc\Exception\JsonRpcException;
+use Drupal\jsonrpc\Object\Error;
+use Drupal\jsonrpc\Object\ParameterBag;
+use Drupal\jsonrpc\Plugin\JsonRpcMethodBase;
+use Drupal\user\PermissionHandlerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+
+/**
+ * @JsonRpcMethod(
+ *   id = "user_permissions.add_permission_to_role",
+ *   usage = @Translation("Add the given permission to the specified role."),
+ *   access = {"administer permissions"},
+ *   params = {
+ *     "permission" = @JsonRpcParameter(schema = {"type": "string"}),
+ *     "role" = @JsonRpcParameter(factory = "\Drupal\jsonrpc\ParameterFactory\EntityParameterFactory"),
+ *   }
+ * )
+ */
+class AddPermissionToRole extends UserPermissionsBase {
+
+  /**
+   * @throws \Drupal\jsonrpc\Exception\JsonRpcException
+   */
+  public function execute(ParameterBag $params) {
+    $permission = $params->get('permission')->getValue();
+    /* @var \Drupal\user\RoleInterface $role */
+    $role = $params->get('role');
+    try {
+      $role->grantPermission($permission);
+      $violations = $role->getTypedData()->validate();
+      if ($violations->count() !== 0) {
+        $error = Error::invalidParams(array_map(function (ConstraintViolationInterface $violation) {
+          return $violation->getMessage();
+        }, iterator_to_array($violations)));
+        throw JsonRpcException::fromError($error);
+      }
+      return $role->save();
+    }
+    catch (EntityStorageException $e) {
+      $error = Error::internalError('Unable to save the user role. Error: ' . $e->getMessage(), $role);
+      throw JsonRpcException::fromError($error);
+    }
+  }
+
+}
